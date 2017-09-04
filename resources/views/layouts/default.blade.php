8bf945710a328ee2b9f8c04e01c6fb8397f3615f
dboyzhang <dboyzhang@gmail.com>
2017-09-04 10:03:07 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title') - laravel static page practice</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
