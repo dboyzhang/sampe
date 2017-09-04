84977ccef26f48ae6d3b3b749f883fe2270dc996
dboyzhang <dboyzhang@gmail.com>
2017-09-04 10:37:33 +0000

    fix conflict

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 @extends('layouts/default')
-=======
-@extends('layouts.default')
->>>>>>> static-pages
 @section('title', 'Help')
 @section('content')
 <h1>Help page</h1>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 @extends('layouts/default')
-=======
-@extends('layouts.default')
->>>>>>> static-pages
 @section('title', 'Home')
 @section('content')
 <h1>Home page</h1>
